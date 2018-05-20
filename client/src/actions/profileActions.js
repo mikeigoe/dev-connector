b8ec19e717b8b088ad25af11914b3c1a27b9b9aa
Mike Igoe <mikeigoe@gmail.com>
2018-05-20 04:54:13 +0100

    Started Profile display, delete Profile functionality

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -4,6 +4,7 @@ import {
   GET_PROFILE,
   PROFILE_LOADING,
   CLEAR_CURRENT_PROFILE,
+  SET_CURRENT_USER,
   GET_ERRORS
 } from './types';
 
@@ -39,7 +40,27 @@ export const createProfile = (profileData, history) => dispatch => {
     );
 };
 
-// Set profile loading
+// Delete account and profile
+export const deleteAccount = () => dispatch => {
+  if (window.confirm('Are you sure? This can NOT be undone!')) {
+    axios
+      .delete('/api/profile')
+      .then(res =>
+        dispatch({
+          type: SET_CURRENT_USER,
+          payload: {}
+        })
+      )
+      .catch(err =>
+        dispatch({
+          type: GET_ERRORS,
+          payload: err.response.data
+        })
+      );
+  }
+};
+
+// Profile loading
 export const setProfileLoading = () => {
   return {
     type: PROFILE_LOADING
